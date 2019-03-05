eaf5d347a25b78ee6f88786001a0ffadb20579d3
fausto bischoff <fabischoff@gmail.com>
2019-03-05 13:17:27 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> Título da página </title>
+    </head>
+    <body>
+        <?php header('Location: html/index.php') ; ?>
+    </body>
+</html>
