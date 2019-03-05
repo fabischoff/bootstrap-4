b277f9a66f7efa3218d81c03220c17852d951b32
fausto bischoff <fabischoff@gmail.com>
2019-03-05 16:52:59 -0300

    fim do capitulo 2

---
+++ html/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <title> Better - Fotos de graça e pagas </title>
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        <link rel="stylesheet" href="../css/better-tema.css">
+
+    </head>
+    <body>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+            <div class="container">
+                <a href="#" class="navbar-brand">Better</a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                                Descubra
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#" class="dropdow-item">Paisagem</a></li>
+                                <li><a href="#" class="dropdow-item">Tecnologia</a></li>
+                                <li><a href="#" class="dropdow-item">Abstrato</a></li>
+                                <li class="dropdown-divider"></li>
+                                <li><a href="#" class="dropdow-item">Animais</a></li>
+                                <li><a href="#" class="dropdow-item">Comida</a></li>
+                                <li><a href="#" class="dropdow-item">Pessoas</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#" class="nav-link">Preços</a></li>
+                        <li><a href="#" class="nav-link">Cadastre-se</a></li>
+                        <li><a href="#" class="nav-link">Login</a></li>
+                    </ul>
+                </div><!--fim div class collapse-->
+            </div><!--fim div class container-->
+        </nav>
+
+
+        <main class="jumbotron">
+            <div class="container">
+                <h1>Momentos perfeitos</h1>
+                <p>Lore ipsum dolor</p>
+
+                <form class="input-group">
+                    <input type="search" class="form-control" placeholder="Encontre o que você procura ...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary"> Pesquisar</button>
+                    </span>
+
+                </form>
+                   
+
+            </div>
+        </main>
+        Buscar
+
+
+        <script src="../js/jquery-3.3.1.js"></script>
+        <script src="../js/bootstrap.js"></script>
+    </body>
+</html>
